73a9b9ab7cd1095430870ab0ba89ca4e3a88c69d
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 10:36:17 +0100

    test setup

---
+++ src/test/java/com/bae/persistence/repositoryTests/TraineeMapRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.bae.persistence.repositoryTests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TraineeMapRepositoryTest {
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void returnNoTraineesWhenMapIsEmpty() {
+
+	}
+
+	@Test
+	public void returnTraineesWhenMapIsFilled() {
+
+	}
+
+}
