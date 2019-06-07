c2c3e80b7b4024be4f4294a0f523411d308de511
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 10:40:40 +0100

    further test setup

--- src/test/java/com/bae/persistence/repositoryTests/TraineeMapRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTests/TraineeMapRepositoryTest.java
@@ -1,23 +1,31 @@
 package com.bae.persistence.repositoryTests;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.bae.persistence.repository.TraineeMapRepository;
+
 public class TraineeMapRepositoryTest {
+
+	private TraineeMapRepository traineeMapRepository;
 
 	@Before
 	public void setup() {
+
+		traineeMapRepository = new TraineeMapRepository();
 
 	}
 
 	@Test
 	public void returnNoTraineesWhenMapIsEmpty() {
-
+		fail("TODO");
 	}
 
 	@Test
 	public void returnTraineesWhenMapIsFilled() {
-
+		fail("TODO");
 	}
 
 }
