473148b383b89b9d434f5a3c0637eebcbacb809a
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 14:30:00 +0100

    writing tests

--- src/test/java/com/bae/persistence/repositoryTests/ClassroomMapRepositoryTest.java
+++ src/test/java/com/bae/persistence/repositoryTests/ClassroomMapRepositoryTest.java
@@ -1,50 +1,86 @@
 package com.bae.persistence.repositoryTests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.bae.persistence.repository.ClassroomMapRepository;
+
 public class ClassroomMapRepositoryTest {
+
+	private ClassroomMapRepository classroomMapRepository;
 
 	@Before
 	public void setup() {
 
+		classroomMapRepository = new ClassroomMapRepository();
 	}
 
 	@Test
 	public void returnNoClassroomsWhenMapIsEmpty() {
-		fail("TODO");
+
+		assertEquals(0, classroomMapRepository.getClassroomMap().size());
+		assertEquals("{}", classroomMapRepository.getAllClassrooms());
 	}
 
 	@Test
 	public void returnClassroomsWhenMapIsFilled() {
-		fail("TODO");
+		classroomMapRepository.getClassroomMap().put(1, trainee1);
+
+		assertEquals(1, classroomMapRepository.getClassroomMap().size());
+		assertEquals("{\"1\":" + trainee1JSON + "}", classroomMapRepository.getAllTrainees());
 	}
 
-	@Test
-	public void addAClassroom() {
-		fail("TODO");
-	}
-
-	@Test
-	public void add2Classrooms() {
-		fail("TODO");
-	}
-
-	@Test
-	public void removeAClassroom() {
-		fail("TODO");
-	}
-
-	@Test
-	public void remove2Classrooms() {
-		fail("TODO");
-	}
-
-	@Test
-	public void updateAClassroom() {
-		fail("TODO");
-	}
+	// @Test
+	// public void addAClassroom() {
+	// classroomMapRepository.addATrainee(trainee1JSON);
+	//
+	// assertEquals(1, classroomMapRepository.getClassroomMap().size());
+	// assertTrue(classroomMapRepository.getClassroomMap().get(1).getFirstName().equals("John"));
+	// }
+	//
+	// @Test
+	// public void add2Classrooms() {
+	// classroomMapRepository.addATrainee(trainee1JSON);
+	// classroomMapRepository.addATrainee(trainee2JSON);
+	//
+	// assertEquals(2, classroomMapRepository.getClassroomMap().size());
+	// assertTrue(classroomMapRepository.getClassroomMap().get(1).getFirstName().equals("John"));
+	// assertTrue(classroomMapRepository.getClassroomMap().get(2).getFirstName().equals("Jane"));
+	// }
+	//
+	// @Test
+	// public void removeAClassroom() {
+	// classroomMapRepository.addATrainee(trainee1JSON);
+	//
+	// classroomMapRepository.removeATrainee(1);
+	//
+	// assertEquals(0, classroomMapRepository.getClassroomMap().size());
+	// }
+	//
+	// @Test
+	// public void remove2Classrooms() {
+	// classroomMapRepository.addATrainee(trainee1JSON);
+	// classroomMapRepository.addATrainee(trainee2JSON);
+	//
+	// classroomMapRepository.removeATrainee(1);
+	// assertEquals(1, classroomMapRepository.getClassroomMap().size());
+	//
+	// classroomMapRepository.removeATrainee(2);
+	// assertEquals(0, classroomMapRepository.getClassroomMap().size());
+	// }
+	//
+	// @Test
+	// public void updateAClassroom() {
+	// classroomMapRepository.addATrainee(trainee1JSON);
+	// String updatedTrainee =
+	// "{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Doe\"}";
+	//
+	// classroomMapRepository.updateTrainee(1, updatedTrainee);
+	//
+	// assertEquals(1, classroomMapRepository.getClassroomMap().size());
+	// assertTrue(classroomMapRepository.getClassroomMap().get(1).getLastName().equals("Doe"));
+	// }
 
 }
