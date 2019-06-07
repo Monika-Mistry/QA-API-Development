c4832371c14d9049d5aae8c6d3b846475b6bcfd2
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 11:07:04 +0100

    traineeservice implementation added

---
+++ src/main/java/com/bae/business/service/TraineeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bae.business.service;
+
+public class TraineeServiceImpl implements TraineeService {
+
+	@Override
+	public String getAllTrainees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
