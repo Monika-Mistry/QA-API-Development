da0a9dadb79138e0abeb1f19ded894fba11619c6
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 14:24:10 +0100

    adding tests

---
+++ src/main/java/com/bae/persistence/repository/ClassroomMapRepository.java
@@ -0,0 +1,29 @@
+package com.bae.persistence.repository;
+
+public class ClassroomMapRepository implements ClassroomRepository {
+
+	@Override
+	public String addAClassroom(String classroom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAllClassrooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateClassroom(int id, String classroom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String removeAClassroom(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/bae/persistence/repositoryTests/ClassroomMapRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.bae.persistence.repositoryTests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassroomMapRepositoryTest {
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void returnNoClassroomsWhenMapIsEmpty() {
+		fail("TODO");
+	}
+
+	@Test
+	public void returnClassroomsWhenMapIsFilled() {
+		fail("TODO");
+	}
+
+	@Test
+	public void addAClassroom() {
+		fail("TODO");
+	}
+
+	@Test
+	public void add2Classrooms() {
+		fail("TODO");
+	}
+
+	@Test
+	public void removeAClassroom() {
+		fail("TODO");
+	}
+
+	@Test
+	public void remove2Classrooms() {
+		fail("TODO");
+	}
+
+	@Test
+	public void updateAClassroom() {
+		fail("TODO");
+	}
+
+}
