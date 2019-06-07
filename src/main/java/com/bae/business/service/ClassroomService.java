d9d4cf88e2f6deebc2fbf30c2cdd673505c3770b
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 14:52:29 +0100

    classroomservice added

---
+++ src/main/java/com/bae/business/service/ClassroomService.java
@@ -0,0 +1,19 @@
+package com.bae.business.service;
+
+public interface ClassroomService {
+
+	// C
+	String addAClassroom(String classroom);
+
+	// R
+	String getAllClassrooms();
+
+	// String getAClassroom(int id);
+
+	// U
+	String updateClassroom(int id, String classroom);
+
+	// D
+	String removeAClassroom(int id);
+
+}
