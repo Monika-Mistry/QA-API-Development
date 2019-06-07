40eb568d76641665a2c045a9331c285ddf5c1ad7
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 10:29:45 +0100

    trainee entity developed and tested

---
+++ src/main/java/com/bae/persistence/domain/Trainee.java
@@ -0,0 +1,55 @@
+package com.bae.persistence.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trainee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(length = 50)
+	private String firstName;
+	@Column(length = 50)
+	private String lastName;
+
+	public Trainee() {
+
+	}
+
+	public Trainee(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
