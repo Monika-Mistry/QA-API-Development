a1cdd5388dba677ef68f3dc5fe7747d7c9683267
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 11:14:00 +0100

    traineecontroller added

---
+++ src/main/java/com/bae/rest/TraineeController.java
@@ -0,0 +1,13 @@
+package com.bae.rest;
+
+import com.bae.business.service.TraineeService;
+
+public class TraineeController {
+
+	private TraineeService service;
+
+	public String getAllTrainees() {
+		return null;
+
+	}
+}
