8075ff89bb537bae3a341c912429c365d2271393
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-07 15:03:21 +0100

    classroom rest layer added

---
+++ src/main/java/com/bae/rest/ClassroomController.java
@@ -0,0 +1,49 @@
+package com.bae.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.bae.business.service.ClassroomService;
+
+@Path("classroom")
+public class ClassroomController {
+
+	@Inject
+	private ClassroomService service;
+
+	@Path("getAllclassrooms")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllClassrooms() {
+		return service.getAllClassrooms();
+
+	}
+
+	@Path("addAclassroom")
+	@POST
+	@Produces({ "application/json" })
+	public String addAClassroom(String classroom) {
+		return service.addAClassroom(classroom);
+	}
+
+	@Path("removeAclassroom/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String removeAClassroom(@PathParam("id") int id) {
+		return service.removeAClassroom(id);
+	}
+
+	@Path("updateAclassroom/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateAClassroom(@PathParam("id") int id, String classroom) {
+		return service.updateClassroom(id, classroom);
+	}
+
+}
